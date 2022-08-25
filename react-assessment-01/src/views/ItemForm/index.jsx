a2256277260845f42314ea42db59fca418742e34
parindyawijeseskara <parindyawijesekara@gmail.com>
2022-08-25 17:14:18 +0530

    Added item table

--- react-assessment-01/src/views/ItemForm/index.jsx
+++ react-assessment-01/src/views/ItemForm/index.jsx
@@ -3,6 +3,33 @@ import React,{ Component,Fragment } from "react";
 import {styleSheet} from "./style";
 import Tabs from '@mui/material/Tabs';
 import {Tab,Grid,Typography,TextField} from '@mui/material';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+
+function createData(
+    itemCode,
+    description,
+    price,
+    qty
+    
+  ) {
+    return {itemCode,description,price,qty};
+  }
+  
+  const rows = [
+    createData('I001', 'Chocalate',100.0, 24),
+    createData('I002', 'Ice-cream', 150.0, 30),
+    createData('I003', 'Dhal', 200.0, 50),
+    createData('I004', 'Eggs',180.0,40)
+  ];
+
+
 
 
 class ItemForm extends Component{
@@ -57,6 +84,34 @@ class ItemForm extends Component{
                         </Grid>
                     </Fragment>
                 </div>
+
+                <TableContainer component={Paper}>
+                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                        <TableHead>
+                        <TableRow>
+                            <TableCell align="center">Item Code</TableCell>
+                            <TableCell align="center">Description</TableCell>
+                            <TableCell align="center">Price</TableCell>
+                            <TableCell align="center">Qty</TableCell>
+                        </TableRow>
+                        </TableHead>
+                        <TableBody>
+                        {rows.map((row) => (
+                            <TableRow
+                            key={row.itemCode}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                            <TableCell component="th" scope="row">
+                                {row.itemCode}
+                            </TableCell>
+                            <TableCell align="center">{row.description}</TableCell>
+                            <TableCell align="center">{row.price}</TableCell>
+                            <TableCell align="center">{row.qty}</TableCell>
+                            </TableRow>
+                        ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
             </div>
         )
     }
